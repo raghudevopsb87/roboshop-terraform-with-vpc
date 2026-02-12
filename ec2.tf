5cd95434556e57e48aa3b4750405af50d8529240
Raghu K <raghuk.vit@gmail.com>
2026-02-12 07:56:41 +0530

    Roboshop project

--- ec2.tf
+++ ec2.tf
@@ -36,6 +36,7 @@ resource "aws_security_group_rule" "allow_all" {
   protocol          = "-1"
   from_port         = 0
   security_group_id = aws_security_group.instances[each.key].id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_instance" "instances" {
